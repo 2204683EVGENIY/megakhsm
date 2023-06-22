8b4730983d74790250d7072b35b774c1ddaa7344
Evgeniy <Vologzhanin2018@gmail.com>
2023-06-22 22:50:27 +0500

    correction remarks

--- spec/features/user_looking_alien_profile_spec.rb
+++ spec/features/user_looking_alien_profile_spec.rb
@@ -5,8 +5,9 @@ require 'rails_helper'
 RSpec.feature 'USER looking alien profile', type: :feature do
   let!(:first_user) { FactoryBot.create :user }
   let!(:second_user) { FactoryBot.create :user }
-  let!(:game_w_questions) { FactoryBot.create(:game_with_questions, user: first_user) }
+  let!(:game_w_questions) { FactoryBot.create(:game_with_questions, user: first_user, current_level: 5, created_at: Time.zone.parse('2012.12.12, 12:00')) }
 
+  before { game_w_questions.take_money! }
   before { login_as second_user }
 
   scenario 'successfully' do
@@ -14,15 +15,26 @@ RSpec.feature 'USER looking alien profile', type: :feature do
 
     click_link first_user.name
 
-    expect(page).to have_current_path '/users/1'
+    expect(page).to have_current_path "/users/#{game_w_questions.user.id}"
 
     expect(page).to have_content first_user.name
 
+    expect(page).to have_content 'Выйти'
+    expect(page).to have_content 'Новая игра'
+    expect(page).to have_content(first_user.name)
+    expect(page).to have_content(game_w_questions.user.name)
+    expect(page).to have_content(game_w_questions.current_level)
+    expect(page).to have_content(game_w_questions.id)
     expect(page).to have_content 'Дата'
+    expect(page).to have_content '12 дек., 12:00'
+    expect(page).to have_content '50/50'
+    expect(page).to have_content 'время'
     expect(page).to have_content 'Вопрос'
     expect(page).to have_content 'Выигрыш'
     expect(page).to have_content 'Подсказки'
 
     expect(page).not_to have_content 'Сменить имя и пароль'
+
+    save_and_open_page
   end
 end
