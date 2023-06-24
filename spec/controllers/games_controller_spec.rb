4c4eeb4792fe5b2d3f7200617975236f3112b3f0
Evgeniy <Vologzhanin2018@gmail.com>
2023-06-24 15:58:08 +0500

    correction remarks

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe GamesController, type: :controller do
     context 'user is not login' do
       before { get :show, id: game_w_questions.id }
 
-      it 'check status' do
+      it 'makes an HTTP response with a status no 200' do
         expect(response.status).not_to eq(200)
       end
 
@@ -55,7 +55,7 @@ RSpec.describe GamesController, type: :controller do
           expect(game.user).to eq(user)
         end
 
-        it 'check status' do
+        it 'makes an HTTP response with a status 200' do
           expect(response.status).to eq(200) # должен быть ответ HTTP 200
         end
 
@@ -73,7 +73,7 @@ RSpec.describe GamesController, type: :controller do
           expect(response).to redirect_to(root_path)
         end
 
-        it 'check status' do
+        it 'makes an HTTP response with a status no 200' do
           expect(response.status).not_to eq(200)
         end
 
@@ -109,7 +109,7 @@ RSpec.describe GamesController, type: :controller do
         expect(flash[:alert]).to be
       end
 
-      it 'check status' do
+      it 'makes an HTTP response with a status no 200' do
         expect(response.status).not_to eq(200)
       end
     end
@@ -186,7 +186,7 @@ RSpec.describe GamesController, type: :controller do
         expect(flash[:alert]).to be
       end
 
-      it 'sets response status not 200' do
+      it 'sets makes an HTTP response with a status no 200' do
         expect(response.status).not_to eq(200)
       end
     end
@@ -270,7 +270,7 @@ RSpec.describe GamesController, type: :controller do
           expect(flash[:alert]).to be
         end
 
-        it 'sets response status not 200' do
+        it 'sets makes an HTTP response with a status no 200' do
           expect(response.status).not_to eq(200)
         end
       end
@@ -393,7 +393,7 @@ RSpec.describe GamesController, type: :controller do
         expect(flash[:alert]).to be
       end
 
-      it 'response status not 200' do
+      it 'makes an HTTP response with a status no 200' do
         expect(response.status).not_to eq(200)
       end
     end
@@ -415,11 +415,11 @@ RSpec.describe GamesController, type: :controller do
         expect(game.finished?).to be true
       end
 
-      it 'get prize' do
+      it 'makes an HTTP response with a status 200' do
         expect(game.prize).to eq(200)
       end
 
-      it 'update user balance' do
+      it 'makes an HTTP response with a status 200' do
         user.reload
         expect(user.balance).to eq(200)
       end
